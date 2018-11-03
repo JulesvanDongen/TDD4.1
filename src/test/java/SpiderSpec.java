fc3a7a3e0784df7c79bc23b0afda97d9efa4d970
Jules van Dongen <julesvandongen@gmail.com>
2018-11-03 23:38:00 +0100

    Implemented test for 10b

--- src/test/java/SpiderSpec.java
+++ src/test/java/SpiderSpec.java
@@ -7,9 +7,7 @@ import java.util.HashMap;
 import java.util.Set;
 import java.util.Stack;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SpiderSpec {
     @Test
@@ -40,9 +38,24 @@ public class SpiderSpec {
 
     @Test
     @Tag("10b")
-    @Disabled
     void whenSpiderMovedToOwnSpaceThenThrowIllegalMoveException() {
+        HashMap<Position, Stack<Tile>> map = new HashMap<>();
 
+        Stack<Tile> a = new Stack<>();
+        a.push(new Spider(Hive.Player.BLACK));
+        map.put(new Position(0, 0), a);
+
+        Stack<Tile> b = new Stack<>();
+        Spider spider = new Spider(Hive.Player.WHITE);
+        b.push(spider);
+        Position spiderPosition = new Position(0, 1);
+        map.put(spiderPosition, b);
+
+        Board board = new Board(map);
+
+        assertThrows(Hive.IllegalMove.class, () -> {
+            board.moveTile(spiderPosition, spiderPosition);
+        });
     }
 
     @Test
