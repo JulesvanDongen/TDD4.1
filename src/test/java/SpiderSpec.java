1c368057c5822b16f581a4bc0119af99d73fdfb5
Jules van Dongen <julesvandongen@gmail.com>
2018-11-03 21:41:24 +0100

    Added SpiderSpec for testing

---
+++ src/test/java/SpiderSpec.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class SpiderSpec {
+    @Test
+    @Tag("10a")
+    @Disabled
+    void whenSpiderMovesThenSlidesThreeTimes() {
+
+    }
+
+    @Test
+    @Tag("10b")
+    @Disabled
+    void whenSpiderMovedToOwnSpaceThenThrowIllegalMoveException() {
+
+    }
+
+    @Test
+    @Tag("10c")
+    @Disabled
+    void whenSpiderMovedThenOnlyMovesOverEmptyTiles() {
+
+    }
+
+    @Test
+    @Tag("10d")
+    @Disabled
+    void whenSpiderMovedThenCannotMoveToSpotBefore() {
+
+    }
+}
